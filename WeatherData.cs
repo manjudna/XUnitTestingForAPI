345821871e9160a9ea5e57faf8708305e7c8d76f
Manjesh Babu <manjudna@gmail.com>
2020-06-11 11:41:22 +0100

    Added intergration tests with AAA pattern

---
+++ WeatherData.cs
@@ -0,0 +1,23 @@
+﻿//using AspNetCore.Mvc;
+
+namespace WeatherDataTestProject
+{
+    public class WeatherData
+    {
+        public int TemperatureC { get; set; }
+
+        public int TemperatureMax { get; set; }
+
+        public int TemperatureMin { get; set; }
+
+        public int Pressure { get; set; }
+
+        public int Humidity { get; set; }
+
+        public int Sunrise { get; set; }
+
+        public int Sunset { get; set; }
+        public string LocationName { get; set; }
+
+    }
+}
--- WeatherDataControllerTests.cs
+++ WeatherDataControllerTests.cs
@@ -1,34 +1,90 @@
+//using AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
-using OpenWeatherMapApi;
-using OpenWeatherMapApi.Controllers;
-using OpenWeatherMapApi.Services;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WeatherDataTestProject
 {
-    public class WeatherDataControllerTests
+    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<OpenWeatherMapApi.Startup>>
     {
+        public HttpClient Client { get; }
 
-        WeatherForecastController _controller;
-        IWeatherService _service;
-
-        public WeatherDataControllerTests()
+        public WeatherForecastControllerTests(WebApplicationFactory<OpenWeatherMapApi.Startup> fixture)
         {
-            _service = new WeatherService(null,null);
-            _controller = new WeatherForecastController(null,_service);
+            //Arrange
+            Client = fixture.CreateClient();
         }
 
         [Fact]
-        public void Get_WhenCalled_ReturnsOkResult()
+        public async Task Get_Should_ReceiveOK_Forecast()
         {
-            // Act
-            var okResult = _controller.GetWeatherData("London");
+            //Act
+            var response = await Client.GetAsync("/WeatherData/London");
 
-            // Assert
-            Assert.IsType<OkObjectResult>(okResult.Result);
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
         }
 
+        [Fact]
+        public async Task Get_Should_Receive_Data_Forecast()
+        {
+            //Act
+            var response = await Client.GetAsync("/WeatherData/London");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var forecast = JsonConvert.DeserializeObject<WeatherData>(await response.Content.ReadAsStringAsync());
+            Assert.Equal("London", forecast.LocationName);
+        }
+
+        [Fact]
+        public async Task Get_Should_ReceiveData_NotNULL_Forecast()
+        {
+            //Act
+            var response = await Client.GetAsync("/WeatherData/London");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var forecast = JsonConvert.DeserializeObject<WeatherData>(await response.Content.ReadAsStringAsync());
+            Assert.NotNull(response);
+            Assert.Equal("London", forecast.LocationName);
+            Assert.NotNull(forecast.Humidity.ToString());
+            Assert.NotNull(forecast.Sunrise.ToString());
+            Assert.NotNull(forecast.Sunset.ToString());
+            Assert.NotNull(forecast.Pressure.ToString());
+            Assert.NotNull(forecast.TemperatureC.ToString());
+            Assert.NotNull(forecast.TemperatureMax.ToString());
+            Assert.NotNull(forecast.TemperatureMin.ToString());
+        }
+
+
+        [Fact]
+        public async Task Get_Should_ThrowNotFound_Forecast()
+        {
+            //Act
+            var response = await Client.GetAsync("/WeatherData/sdfsddsdf");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Should_ThrowBadRequest_Forecast()
+        {
+            //Act
+            var response = await Client.GetAsync("/WeatherData/*&*&2d");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+        }
 
     }
 }
